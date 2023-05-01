35e73aa3c917531ec2a6524c09ea860020b48a00
fede <federicoscheytt@gmail.com>
2023-05-01 11:55:23 -0300

    First commit

---
+++ TP1/function_P1.m
@@ -0,0 +1,32 @@
+function [x1, x2] = f_P1(x1i, x2i, T, e, tf)
+% f_P1(x1i, x2i, T, e, tf): Funcion que ...
+%
+% INPUT: 
+%   x1i: 
+%   x2i: 
+%   T:   
+%   e:   
+%   tf:  
+%
+% OUTPUT:
+%   x1: 
+%   x2: 
+
+% Author: Federcio Scheytt - Joaquin Gonzalez Targon
+% Date: Mayo 2023
+    
+    % Discretizacion
+    t = 0:T:tf-T;
+    
+    % Condiciones iniciales
+    x1 = [x1i, zeros(1,length(t)-2)];
+    x2 = [x2i, zeros(1,length(t)-2)];
+    
+    % Aproximacion de Euler
+    for n = 1:length(t)-1
+        x1(n+1) = x1(n) + T.*x2(n);
+        x2(n+1) = x2(n) + T.*(e.*((1-(x1(n)).^2).*x2(n)) - x1(n));
+    end
+
+    return
+end
---
+++ TP1/script_P1_c.m
@@ -0,0 +1,46 @@
+% Script: s_P1_c.m
+% Author: Federico, Scheytt - Joaquin, Gonzalez Targon
+% Date: Mayo 2023
+
+clear, clc, close all
+
+% Constantes 
+T = [0.001 0.1];
+e = [0 1 10 50];
+tf = 100;
+
+% Condiciones iniciales
+x1i = 0;
+x2i = 1;
+
+% Implementacion del metodo
+for i = 1:length(T)
+    % Discretizacion
+    t = 0:T(i):tf-T(i);
+    x1 = [x1i, zeros(1,length(t)-2)];
+    x2 = [x2i, zeros(1,length(t)-2)];
+    for j = 1:length(e)
+        for n = 1:length(t)-1
+            % Aproximacion de Euler
+            x1(n+1) = x1(n) + T(i).*x2(n);
+            x2(n+1) = x2(n) + T(i).*(e(j).*((1-(x1(n)).^2).*x2(n)) - x1(n));
+        end
+        f_plot(i,j,x1,x2,e,t);
+    end 
+end
+
+function f_plot(i,j,x1,x2,e,t)
+    figure(i)
+        subplot(2,4,j), plot(t,x1,'r')
+            title(['$\varepsilon$ = ', num2str(e(j))],'Interpreter','latex')
+            legend('x_{1}(t)', 'Location', 'northwest')
+            xlabel('Tiempo [seg]')
+            ylabel('x_{1}(t)')
+            hold on, grid on
+        subplot(2,4,j+4), plot(t,x2,'b')
+            title(['$\varepsilon$ = ', num2str(e(j))],'Interpreter','latex')
+            legend('x_{2}(t)', 'Location', 'northwest')
+            xlabel('Tiempo [seg]')
+            ylabel('x_{2}(t)')
+            hold on, grid on
+end
